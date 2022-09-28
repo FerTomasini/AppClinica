81bfc0053cc8f8afaca6ffa00e1b3eab8afa5a7d
FerTomasini <fer.tomasini@gmail.com>
2022-09-27 21:05:31 -0300

    Initial Commit

---
+++ src/main/java/com/example/appclinica/AppClinicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.appclinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppClinicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppClinicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appclinica/AppClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppClinicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
